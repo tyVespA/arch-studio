360cc2c55ead6fa4f47bdc1c50e69275efe8fa95
tyVespA <migliavacca.marco08@gmail.com>
2023-05-09 10:14:33 +0200

    Create navbar
    todo: menu button on mobile

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,5 +1,32 @@
 import React from "react";
+import Link from "next/link";
+import Image from "next/image";
+import styles from "./Navbar.module.css";
 
 export default function Navbar() {
-  return <div>navbar</div>;
+  return (
+    <div className={styles.header}>
+      <Link href="#">
+        <Image
+          src="/logo.svg"
+          width={97}
+          height={40}
+          alt="logo della compagnia"
+        />
+      </Link>
+      <button className={styles.hamburger}>
+        <Image
+          src="/images/icons/icon-hamburger.svg"
+          width={24}
+          height={17}
+          alt="bottone menu"
+        />
+      </button>
+      <nav className={styles.navbar}>
+        <Link href="">Portfolio</Link>
+        <Link href="">About Us</Link>
+        <Link href="">Contact</Link>
+      </nav>
+    </div>
+  );
 }
