a62cd34aa8ff453f830c15f77afd6c8ebbd43512
tyVespA <migliavacca.marco08@gmail.com>
2023-05-09 05:28:14 +0200

    Setup pages

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,15 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import Link from "next/link";
+import Navbar from "@/components/Navbar";
 
 export default function Home() {
-  return <main>test</main>;
+  return (
+    <main>
+      <Navbar />
+      <h1>test</h1>
+      <Link href="portfolio">Portfolio</Link>
+      <Link href="portfolio/test">Portfolio test</Link>
+    </main>
+  );
 }
