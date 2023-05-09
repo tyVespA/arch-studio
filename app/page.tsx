fc1cfbd4252e0088494531f9bd857943c8d597d0
tyVespA <migliavacca.marco08@gmail.com>
2023-05-09 09:11:40 +0200

    Add font using@next/font

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,17 @@
 import Image from "next/image";
-import styles from "./page.module.css";
 import Link from "next/link";
+import styles from "./page.module.css";
 import Navbar from "@/components/Navbar";
+import { League_Spartan } from "@next/font/google";
+
+const leagueSpartan = League_Spartan({
+  subsets: ["latin"],
+  weight: ["500", "700"],
+});
 
 export default function Home() {
   return (
-    <main>
+    <main className={leagueSpartan.className}>
       <Navbar />
       <h1>test</h1>
       <Link href="portfolio">Portfolio</Link>
