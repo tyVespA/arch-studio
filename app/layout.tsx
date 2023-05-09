7d4973469d59157dd335a69bbdaf48510823ffc1
tyVespA <migliavacca.marco08@gmail.com>
2023-05-09 09:11:10 +0200

    Add global layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import "./globals.css";
+import styles from "./layout.module.css";
 
 export const metadata = {
   title: "Create Next App",
@@ -12,7 +13,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={styles.layout}>{children}</body>
     </html>
   );
 }
