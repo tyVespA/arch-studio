c5208a0aeb2c69a5d4a593ca590c9d6bb750f55f
tyVespA <migliavacca.marco08@gmail.com>
2023-05-10 17:41:10 +0200

    Complete header section

--- app/page.tsx
+++ app/page.tsx
@@ -16,7 +16,42 @@ export default function Home() {
     <main className={leagueSpartan.className}>
       <PageName pageName="Home" aria-hidden="true" />
       <Navbar />
-      <Hero />
+      <header className={styles.header}>
+        <Hero />
+      </header>
+      <section className={styles.welcomeSection}>
+        <div>
+          <h1>
+            Welcome to <br />
+            Arch Studio
+          </h1>
+          <p>
+            We have a unique network and skillset to help bring your projects to
+            life. Our small team of highly skilled individuals combined with our
+            large network put us in a strong position to deliver exceptional
+            results.
+          </p>
+          <p>
+            Over the past 10 years, we have worked on all kinds of projects.
+            From stations to high-rise buildings, we create spaces that inspire
+            and delight.
+          </p>
+          <p>
+            We work closely with our clients so that we understand the
+            intricacies of each project. This allows us to work in harmony the
+            surrounding area to create truly stunning projects that will stand
+            the test of time.
+          </p>
+        </div>
+        <div>
+          <Image
+            src="/images/home/desktop/image-welcome.jpg"
+            width={350}
+            height={568}
+            alt="dettaglio di un edificio"
+          />
+        </div>
+      </section>
       <h1>test</h1>
       <Link href="portfolio">Portfolio</Link>
       <Link href="portfolio/test">Portfolio test</Link>
