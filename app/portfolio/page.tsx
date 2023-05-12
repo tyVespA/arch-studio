89d9e473cd99cf3240b3a79faa086ed03834c529
tyVespA <migliavacca.marco08@gmail.com>
2023-05-12 04:50:35 +0200

    Create and style portfolio

--- app/portfolio/page.tsx
+++ app/portfolio/page.tsx
@@ -1,7 +1,8 @@
 import React from "react";
+import styles from "./page.module.css";
 import PageName from "@/components/PageName";
 import Navbar from "@/components/Navbar";
-import Hero from "@/components/Hero";
+import FeaturedCard from "@/components/FeaturedCard";
 
 export default function portfolio() {
   return (
@@ -10,6 +11,92 @@ export default function portfolio() {
         <PageName pageName="Portfolio" aria-hidden="true" />
         <Navbar />
       </header>
+      <main className={styles.portfolioGrid}>
+        <FeaturedCard
+          imageName="image-seraph"
+          projectName="Seraph Station"
+          anchorText=""
+          number=""
+          date="September 2019"
+        />
+        <FeaturedCard
+          imageName="image-eebox"
+          projectName="Eebox Building"
+          anchorText=""
+          number=""
+          date="August 2017"
+        />
+        <FeaturedCard
+          imageName="image-federal"
+          projectName="Federal II Tower"
+          anchorText=""
+          number=""
+          date="March 2017"
+        />
+        <FeaturedCard
+          imageName="image-del-sol"
+          projectName="Image Del Sol"
+          anchorText=""
+          number=""
+          date="January 2016"
+        />
+        <FeaturedCard
+          imageName="image-prototype"
+          projectName="Le Prototype"
+          anchorText=""
+          number=""
+          date="October 2015"
+        />
+        <FeaturedCard
+          imageName="image-228B"
+          projectName="228B Tower"
+          anchorText=""
+          number=""
+          date="April 2015"
+        />
+        <FeaturedCard
+          imageName="image-edelweiss"
+          projectName="Grand Edelweiss Hotel"
+          anchorText=""
+          number=""
+          date="December 2013"
+        />
+        <FeaturedCard
+          imageName="image-netcry"
+          projectName="Netcry Tower"
+          anchorText=""
+          number=""
+          date="August 2012"
+        />
+        <FeaturedCard
+          imageName="image-hypers"
+          projectName="Hypers"
+          anchorText=""
+          number=""
+          date="January 2012"
+        />
+        <FeaturedCard
+          imageName="image-sxiv"
+          projectName="SXIV Tower"
+          anchorText=""
+          number=""
+          date="March 2011"
+        />
+        <FeaturedCard
+          imageName="image-trinity"
+          projectName="Trinity Bank Tower"
+          anchorText=""
+          number=""
+          date="September 2010"
+        />
+        <FeaturedCard
+          imageName="image-paramour"
+          projectName="Project Paramour"
+          anchorText=""
+          number=""
+          date="February 2008"
+        />
+      </main>
     </div>
   );
 }
